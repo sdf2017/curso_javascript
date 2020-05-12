292eaa38459650cc4759d9fa83ea5381f40521e9
Silvio Dias <silviodias.rj@gmail.com>
2020-05-12 03:05:35 -0400

    Meu segundo commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//var nome = "Silvio Dias";
+//var frases = "Eu amo jogar futebol!"
+//alert("Bemvindo "+nome);
+//console.log(nome);
+//console.log(frases);
+/*
+var lista = ["Uva", "Maçã", "Pera", "Laranja"];
+lista.push("Banana");
+console.log(lista.push("Goiaba")); //push - adciona elemento no array
+console.log(lista.pop()); //pop - retira o ultimo elemento do array
+console.log(lista.length); // length - exibe o tamanho do array
+//lista.reverse;
+console.log(lista.toString());
+console.log(lista.join(" - "));
+console.log(lista);
+
+var pessoa = {nome: "Silvio", sobrenome: "Dias Ferreira", idade: 44};
+console.log(pessoa);
+*/
+/*
+var frutas = [{nome: "Maçã", cor: "Vermelha"}, {nome:"Uva", cor:"Roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+*/
+var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("Seja bem vindo, Pode acessar este conteúdo!");
+}else{
+    alert("Você não pode acessar este conteúdo, pois é menor de idade!");
+}
